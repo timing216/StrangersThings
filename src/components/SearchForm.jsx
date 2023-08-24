5f181985e21ac5e452cfffad76ba827e4dfcc3c5
tealdobey <127458303+tealdobey@users.noreply.github.com>
2023-08-24 05:10:28 -0500

    search form

---
+++ src/components/SearchForm.jsx
@@ -0,0 +1,25 @@
+import React, {useState} from 'react';
+
+const SearchForms = ({ posts, setFilteredPosts }) => {
+  const[keyword, setKeyword] = useState('')
+  const handleChange = (event) => { setKeyword(event.target.value)}
+  const handleSubmit = (event) => {event.preventDefult();
+    const resultsArray = posts.filter(post => {
+      const title = post.title.toLowerCase();
+      const description = post.description.toLowerCase();
+      return title.includes(keyword.toLowerCase()) || description.includes(keyword.toLowerCase())
+    })
+
+    setFilteredPosts(resultsArray)
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+      type='text'
+      onChange={(handleChange)}
+      />
+      <button type='submit'>Search</button>
+    </form>
+  )
+}
+export default SearchForms;
