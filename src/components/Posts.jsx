de90917da03cce2d9a41c55b0ff0c498a42fd06a
jrodri9474 <jacobrodriguez191@yahoo.com>
2023-08-24 14:25:49 -0700

    delete button

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import { ToastContainer, toast } from "react-toastify";
 import { getPosts } from "../API";
 import Register from "./Register";
 import { registerUser } from "../API";
